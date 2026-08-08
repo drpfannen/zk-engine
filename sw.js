3f17572e46de81770e8a11a9f745d672b37c4662
Keith Pfannenstiel <keith_pfannenstiel@yahoo.com>
2026-08-07 19:17:22 -0500

    fix icon plate ordering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zk-engine-c2d481b7';
+const CACHE='zk-engine-35d6d63f';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
